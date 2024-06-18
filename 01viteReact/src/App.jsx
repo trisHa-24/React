4893a4237f8d299705fef6413ab208c348f9bbdb
trisHa-24 <mt123456trisha@gmail.com>
2024-06-18 13:18:00 +0530

    custom react

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -2,10 +2,11 @@
 
 function App() {
  
-
+   const username ="evaluated Expression"
   return (
     <>
       <h1>Learn React</h1>
+      <p>this is how we inject variable {username}</p>
     </>
   )
 }
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+      <div>
+        <h1>Custom App !</h1>
+      </div>
+    )
+}
 
+//behind the scene every function converted into this structure -> (customReact for more info)
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherElement =  (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+
+)
+
+
+const anotherUser = "variable injected "
+
+const reactElement = React.createElement(
+    'a',{href: 'https://google.com', target:'_blank'},
+    'click me to visit google',
+    anotherUser
+)
+//babble injects createElement
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   //<MyApp/> // MyApp() will also work cause it's a function , behind the scence vite bundler compile the code and modify the code in correct react format
+   // anotherElement
+    reactElement
+
+   //<App/>
 )
