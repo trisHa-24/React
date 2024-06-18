3bf5614891aef403efa561108da7c4b98cc2eeb9
trisHa-24 <mt123456trisha@gmail.com>
2024-06-18 10:11:49 +0530

    file structure

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>hiie</h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Learn React</h1>
+    </>
+  )
+}
+
+export default App
