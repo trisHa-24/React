d31803125108d6c4c3f41a722aa393034671f1d4
trisHa-24 <mt123456trisha@gmail.com>
2024-06-25 09:52:15 +0530

    React-Router

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
