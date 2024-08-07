d2903b3da36af52997cbcfffbe472ed0617c53de
trisHa-24 <mt123456trisha@gmail.com>
2024-08-07 20:00:18 +0530

    megablog

---
+++ 11_Blog/src/pages/EditPost.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react'
+import { Container, PostForm } from '../components'
+import appwriteService from '../appwrite/config'
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [posts, setPosts] = useState(null);
+    const {slug} = useParams();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+       if(slug){
+        appwriteService.getPost(slug)
+        .then((post) => {
+            if(post){
+                setPosts(post);
+            }
+        })
+       } else{
+            navigate('/')
+       }
+    }, [slug,navigate])
+
+    return posts ? (
+        <div>
+            <Container>
+                <PostForm post={posts} />
+            </Container>
+        </div>
+    ) 
+    : null;
+}
+
+export default EditPost
